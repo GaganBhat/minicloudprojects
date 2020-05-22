1967f5a1f25c1460805dd6704fc1c71510361ed2
Gagan Bhat <gaganbhat6@gmail.com>
2020-05-22 15:31:53 -0500

    Working backup verfication for AWS RDS Instances

--- src/main/java/RDSBackupChecker.java
+++ src/main/java/RDSBackupChecker.java
@@ -9,15 +9,16 @@ import java.util.List;
 public class RDSBackupChecker {
 
 	public static void main(String[] args) {
-		checkRDSInstances(new String[]{"database-1", "database-2"});
+		// Some test cases
+		checkRDSInstances(new String[]{"database-1", "database-2"}, 2, "06:20-06:50");
 	}
 
-	public static void checkRDSInstances(String[] instanceIDs){
+	public static void checkRDSInstances(String[] instanceIDs, int retentionPeriod, String backupWindow){
 		for(String instanceID : instanceIDs)
-			checkConfigMatch(instanceID);
+			checkConfigMatch(instanceID, retentionPeriod, backupWindow);
 	}
 
-	public static void checkConfigMatch(String instanceID){
+	public static void checkConfigMatch(String instanceID, int retentionPeriod, String backupWindow){
 		AmazonRDS client = AmazonRDSClientBuilder.standard().build();
 		DescribeDBInstancesRequest request = new DescribeDBInstancesRequest().withDBInstanceIdentifier(instanceID);
 		List<DBInstance> dbInstances = client.describeDBInstances(request).getDBInstances();
@@ -26,11 +27,22 @@ public class RDSBackupChecker {
 			return;
 		}
 		for(DBInstance instance : dbInstances)
-			if(instance.getBackupRetentionPeriod() != 0)
-				System.out.println("Instance " + instance.getDBInstanceIdentifier()
-						+ " has backup enabled with retention period "
-						+ instance.getBackupRetentionPeriod());
-			 else
+			if(instance.getBackupRetentionPeriod() != 0) {
+				StringBuilder builder = new StringBuilder("Instance " + instance.getDBInstanceIdentifier()
+						+ " has backup enabled \n");
+			if(instance.getBackupRetentionPeriod().equals(retentionPeriod))
+				builder
+						.append("Satisfies backup retention periodic of ")
+						.append(instance.getBackupRetentionPeriod())
+						.append("\n");
+
+			if(instance.getPreferredBackupWindow().equals(backupWindow))
+				builder
+						.append("Satisfies backup window of ")
+						.append(instance.getPreferredBackupWindow())
+						.append("\n");
+
+			} else
 				System.out.println("Instance " + instance.getDBInstanceIdentifier() + "backup disabled ");
 
 	}
